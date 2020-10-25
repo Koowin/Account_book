116c4025d963bb7c5ffc8483faa694f048f4dba2
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2020-10-26 01:11:06 +0900

    Update header.hpp

--- Account_book/header.hpp
+++ Account_book/header.hpp
@@ -41,3 +41,9 @@ bool addCategoryList(list <string> *);			//실패 시 True 반환
 bool modifyCategory(list <string> *);				//실패 시 True 반환
 bool deleteCategory(list <string> *,list <class Record> *);		//실패 시 True 반환
 
+/* checkData 함수 */
+bool checkDate(int *date);		//실패시 True 반환
+bool checkMoney(string money);		//실패시 True 반환
+bool checkAmount(int amount);		//실패시 True 반환
+bool checkMemo(string memo);		//실패시 True 반환
+bool checkCategoryNumber(short CategoryNumber);		//실패시 True 반환
