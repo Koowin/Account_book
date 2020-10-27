fbff31c7057d64a4804a76008846b50b94e4d0ec
Koowin <koowin42@gmail.com>
2020-10-27 20:10:43 +0900

    edit categoryFunc and getter setter

--- Account_book/categoryManage.cpp
+++ Account_book/categoryManage.cpp
@@ -1,17 +1,53 @@
 #include "header.hpp"
 
+
+void CategoryManage::categoryMenu(RecordManage& record_manager) {
+	string input_string;
+
+	while (1) {
+		cout << "@ Manage category @" << endl;
+		cout << "1. View categories" << endl;
+		cout << "2. Add new category" << endl;
+		cout << "3. Edit category" << endl;
+		cout << "4. Delete category\n" << endl;
+
+		cout << "Select menu (q:return to main menu)" << endl << "> ";
+		cin >> input_string;
+
+		if (input_string == "q") {
+			break;
+		}
+		else if (input_string == "1") {
+			printCategoryList();
+		}
+		else if (input_string == "2") {
+			addCategory();
+		}
+		else if (input_string == "3") {
+			modifyCategory();
+		}
+		else if (input_string == "4") {
+			deleteCategory(record_manager);
+		}
+		else {
+			cout << "Please enter a valid value" << endl;
+		}
+	}
+}
+
+
 /*  input : list <string> category_table
 *	output : void
 *	main함수에서 category_table을 받아서 모든 카테고리 목록을 출력하는 함수
 */
-void printCategoryList(list <string> *category_table) {
+void CategoryManage::printCategoryList() {
 	int i=1;
-	list <string>::iterator iter;
-	list <string>::iterator end_of_list = category_table->end();
+	list <Category>::iterator iter;
+	list <Category>::iterator end_of_list = category.end();
 
 	cout << "@ View categories @" << endl;
-	for (iter = category_table->begin(); iter != end_of_list; iter++) {
-		cout << i << ". " << *iter << endl;
+	for (iter = category.begin(); iter != end_of_list; iter++) {
+		cout << i << ". " << iter->get_cname() << endl;
 		i++;
 	}
 	cout << "\nPress any key to continue..." << endl;
@@ -23,12 +59,11 @@ void printCategoryList(list <string> *category_table) {
 *	새로운 카테고리를 추가하는 함수
 */
 
-bool addCategoryList(list <string> *category_table) {
+bool CategoryManage::addCategory() {
 	bool flag=true;
 	string input_string;
 	string confirm_string;
-	string to_main_menu;		//'q'를 저장할 문자열
-	to_main_menu.assign(1,'q');
+	CheckerParser cp;
 
 	cout << "@ Add new category @" << endl;
 
@@ -37,21 +72,18 @@ bool addCategoryList(list <string> *category_table) {
 		cin >> input_string;
 
 		//입력이 'q'인지 검사
-		if (input_string.compare("q") == 0) {
+		if (input_string == "q") {
 			return true;
 		}
 		else {
 			//카테고리 문자열 길이 검사
-			if (input_string.size() > 20) {
-				cout << "max category name size = 20" << endl;			//오류 문구 수정 가능
-			}
-			else {
+			if (!cp.checkCategoryName(input_string)) {
 				//카테고리 저장확인
 				cout << "Confirm new category? (type 'No' to cancle)" << endl << "> ";
 				cin >> confirm_string;
-				if (confirm_string.compare("No") != 0) {
+				if (confirm_string != "No") {
 					//카테고리 저장
-					category_table->push_back(input_string);
+					category.push_back(Category(input_string));
 				}
 				flag = false;
 			}
@@ -59,20 +91,23 @@ bool addCategoryList(list <string> *category_table) {
 	}
 	return false;
 }
-bool modifyCategory(list <string> *category_table) {
+
+bool CategoryManage::modifyCategory() {
 	bool flag = true;
 	int selected_num;
 	string input_string;
 	string confirm_string;
+	CheckerParser cp;
 
 	cout << "@ Edit category @" << endl;
 	int i = 1;
-	list <string>::iterator iter;
-	list <string>::iterator end_of_list = category_table->end();
+	list <Category>::iterator iter;
+	list <Category>::iterator end_of_list = category.end();
 
+	//카테고리 출력부
 	cout << "@ View categories @" << endl;
-	for (iter = category_table -> begin(); iter != end_of_list; iter++) {
-		cout << i << ". " << *iter << endl;
+	for (iter = category.begin(); iter != end_of_list; iter++) {
+		cout << i << ". " << iter->get_cname() << endl;
 		i++;
 	}
 
@@ -91,17 +126,18 @@ bool modifyCategory(list <string> *category_table) {
 			cout << "Input only number" << endl;
 		}
 		//0 ~ table size 이외의 입력 예외 처리
-		if (selected_num > category_table->size() || selected_num < 1) {
-			cout << "Number must be over 0 and under " << category_table->size() + 1 << endl;
+		if (selected_num > category.size() || selected_num < 1) {
+			cout << "Number must be over 0 and under " << category.size() + 1 << endl;
 		}
 		else {
 			flag = false;
 		}
 	}
 
-	list<string>::iterator selector = category_table->begin();
+	list<Category>::iterator selector = category.begin();
 	advance(selector, selected_num-1);
-	cout << "\nCategory selected: " << *selector << endl;
+	cout << "\nCategory selected: " << selector->get_cname() << endl;
+
 	while (1) {
 		cout << "Enter category modification (q: return to main menu)" << endl << "> ";
 		cin >> input_string;
@@ -111,12 +147,9 @@ bool modifyCategory(list <string> *category_table) {
 		}
 		else {
 			//input_string check
-			if (input_string.size() > 20) {
-				cout << "max category name size = 20" << endl;				//오류 문구 수정 가능
-			}
-			else {
+			if (!cp.checkCategoryName(input_string)) {
 				//수정 확인
-				cout << "Before modification : " << *selector << endl;
+				cout << "Before modification : " << selector->get_cname() << endl;
 				cout << "After modification : " << input_string << endl;
 				cout << "Confirm modification? (type 'No' to cacel)" << endl << "> ";
 				cin >> confirm_string;
@@ -124,7 +157,7 @@ bool modifyCategory(list <string> *category_table) {
 					return true;
 				}
 				else {
-					selector->assign(input_string);
+					selector->set_cname(input_string);
 					return false;
 				}
 			}
@@ -132,7 +165,7 @@ bool modifyCategory(list <string> *category_table) {
 	}
 }
 
-bool deleteCategory(list <string> *category_table, list <class Record> *record_list) {
+bool CategoryManage::deleteCategory(RecordManage & record_manager) {
 	bool flag = true;
 	bool duplicate_checker = false;
 
@@ -141,11 +174,11 @@ bool deleteCategory(list <string> *category_table, list <class Record> *record_l
 
 	cout << "@ Delete category @" << endl;
 	int i = 1;
-	list <string>::iterator iter;
-	list <string>::iterator end_of_list = category_table->end();
+	list <Category>::iterator iter;
+	list <Category>::iterator end_of_list = category.end();
 
-	for (iter = category_table->begin(); iter != end_of_list; iter++) {
-		cout << i << ". " << *iter << endl;
+	for (iter = category.begin(); iter != end_of_list; iter++) {
+		cout << i << ". " << iter->get_cname() << endl;
 		i++;
 	}
 
@@ -165,18 +198,19 @@ bool deleteCategory(list <string> *category_table, list <class Record> *record_l
 		}
 
 		//0 ~ table size 이외의 입력 예외 처리
-		if (selected_num > category_table->size() || selected_num < 1) {
-			cout << "Number must be over 0 and under " << category_table->size() + 1 << endl;
+		list <Record>::iterator record_iter;
+		list <Record>::iterator end_of_record_list = record_manager.get_end();
+
+		if (selected_num > category.size() || selected_num < 1) {
+			cout << "Number must be over 0 and under " << category.size() + 1 << endl;
 		}
 		else {
 			//to do 예외처리: 해당 카테고리 번호가 존재하는 기록이 있으면 삭제 안되게
-			list <class Record>::iterator record_iter;
-			list <class Record>::iterator end_of_record_list = record_list->end();
 
 			selected_num--;
 
-			for (record_iter = record_list->begin(); record_iter != end_of_record_list; record_iter++) {
-				if (record_iter->category_number == selected_num) {
+			for (record_iter = record_manager.get_first(); record_iter != end_of_record_list; record_iter++) {
+				if (record_iter->get_category_number() == selected_num) {
 					duplicate_checker = true;
 					break;
 				}
@@ -197,11 +231,22 @@ bool deleteCategory(list <string> *category_table, list <class Record> *record_l
 			return true;
 		}
 		else {
+			//모든 기록들 카테고리 번호 하나씩 줄이기 (선택한 것 보다 크다면)
+			for (record_iter = record_manager.get_first(); record_iter != end_of_record_list; record_iter++) {
+				int category_num = record_iter->get_category_number();
+				if (category_num > selected_num+1) {
+					record_iter->set_category_number(category_num-1);
+				}
+			}
 			//삭제 작업
-			iter = category_table->begin();
+			iter = category.begin();
 			advance(iter, selected_num);
-			category_table->erase(iter);
+			category.erase(iter);
 			return false;
 		}
 	}
+}
+
+int CategoryManage::getCategorySize() {
+	return category.size();
 }
